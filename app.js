ac7984efb3f431e2bd7458b391096a959a9393bc
Yash Gosavi <iyashgosavi02@gmail.com>
2024-11-19 18:40:58 +0530

    JSX and Functional Component

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-// importing react and react-dom 
+// importing react and react-dom
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// creating a heading element
+// core react element
+// const heading = React.createElement('h1',{id: "heading"}, "Hello World!");
 
-const heading = React.createElement('h1', {id: 'heading'}, "Hello World");
+// JSX
+const heading = (
+  <h1 id="heading" className="header">
+    Hello JSX!
+  </h1>
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// React Functinal Component
 
-root.render(heading);
+const HeadingComponent = () => {
+    return <h1 id="heading"> Namaste React!</h1>
+}
+
+// const HeadingComponent = () => (
+//     <h1 id="heading"> Namaste React!</h1>
+// )
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(HeadingComponent());
+root.render(<HeadingComponent/>);
+
